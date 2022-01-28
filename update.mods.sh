e27b5910ec39b7f8765f6fe8ef2ae494a6f8a0fd
Joshua <jqshuv@gmail.com>
2022-01-28 17:19:03 +0100

    update file

--- update.mods.sh
+++ update.mods.sh
@@ -2,8 +2,8 @@ git fetch
 git pull
 git pull --tags
 
-rm pax & wget https://github.com/froehlichA/pax/releases/latest/download/pax && sudo chmod 0777 pax
-sudo bash pax -y upgrade
+rm pax & wget -q https://github.com/froehlichA/pax/releases/latest/download/pax && sudo chmod 0777 pax
+sudo ./pax -y upgrade
 
 git add .
 git commit -am "✈️ Updated Mods"
