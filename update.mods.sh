735c6629a67a975a62c7bc7bc94917f780566e42
Joshua <jqshuv@gmail.com>
2022-01-29 02:36:22 +0100

    Removed update file

--- update.mods.sh
+++
@@ -1,9 +0,0 @@
-git fetch
-git pull
-
-rm pax & wget -q https://github.com/froehlichA/pax/releases/latest/download/pax && sudo chmod 0777 pax
-sudo ./pax -y upgrade
-
-git add .
-git commit -am "✈️ Updated Mods"
-git push 
