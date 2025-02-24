483a325a7f90da411e4525d080ff9d75bd3fa5f1
Joshua <schmitt@unately.com>
2025-02-24 23:27:08 +0100

    feat: remove start.sh and test.sh scripts

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -jar pwib.jar -g -s server "$1"
-java -Xms128M -XX:MaxRAMPercentage=95.0 --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:+AlwaysPreTouch $( [[  ! -f unix_args.txt ]] && printf %s "-jar $2" || printf %s "@unix_args.txt" ) --nogui
