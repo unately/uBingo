c4f4101fe9f54e647b17d0340be355ed134ec9df
Joshua <schmitt@unately.com>
2025-02-25 14:07:08 +0100

    feat: add build script and update configuration for release process

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2025 Jqshuv
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+VERSION=$(jq -r '.version' package.json)
+
+mkdir -p build
+
+# Run patcher script
+./scripts/patcher.sh
+
+# Export the modpack
+packwiz modrinth export --output build/uBingo-$VERSION.mrpack
+
+# Export the server
+zip -r build/uBingo-$VERSION-server.zip server/*
